eabab5b132c47739d13aff5c7692c373b2300ad8
Raul Escamilla <raul.escamilla@convergys.com>
2017-08-31 15:47:30 -0600

    Add Ng2KnobDirective to exports on NgModule

--- src/angular2-knob.module.ts
+++ src/angular2-knob.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Ng2KnobDirective} from './angular2-knob.directive';
 
 @NgModule({
+    exports: [Ng2KnobDirective],
     declarations: [Ng2KnobDirective]
 })
 export class KnobModule { }
